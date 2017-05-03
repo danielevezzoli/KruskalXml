2c8afc5720ae8c577ecd0c01e1dd21cb3bf38048
StefanoPoma97 <s.poma001@studenti.unibs.it>
2017-05-03 16:43:15 +0200

    implementato metodo dijkstra

--- src/it/unibs/ing/fp/kruskalxml/Graph.java
+++ src/it/unibs/ing/fp/kruskalxml/Graph.java
@@ -7,9 +7,9 @@ public class Graph {
 
 	private Vector<Node> nodes = new Vector<>();
 	private PriorityQueue<Edge> edges = new PriorityQueue<>();
-	
 	private Vector<Group_String> gruppi = new Vector<>();
 	private Vector<Edge> path = new Vector<>();
+	private Vector<String> pathD = new Vector<>();
 
 	public boolean addNode(Node node){
 		boolean flag = true;
@@ -73,7 +73,7 @@ public class Graph {
 		}
 	}
 	
-	public void Kruskal_String()
+	public void kruskal_String()
 	{
 		
 		aggiornaGruppi();
@@ -127,6 +127,103 @@ public class Graph {
 		
 	}
 	
+	private Vector<Node> settledNodes = new Vector<>();
+	private Vector<Node> unSettledNodes = new Vector<>();
+	private Node endNodo = new Node();
+	private void updateArray()
+	{
+		for (Node n: nodes)
+		{
+			if (n.getStart())
+			{
+				n.setDistance(0);
+				settledNodes.add(n);
+				
+			}
+			
+			else
+			{
+				if(n.getEnd()) endNodo = n;
+				n.setDistance(99);
+				unSettledNodes.add(n);
+			}
+		}
+		
+		for (Node n: settledNodes)
+			System.out.println("settleNodes:"+n);
+		
+		for (Node n: unSettledNodes)
+			System.out.println("unsettleNodes:"+n);
+	}
+	private void updateNearNodes()
+	{
+		for (Edge e: edges)
+		{
+			if(e.getStartNode().equals(settledNodes.get(settledNodes.size()-1)))
+				updateNode(e.getEndNode(),settledNodes.get(settledNodes.size()-1),e.getWeight());
+			if(e.getEndNode().equals(settledNodes.get(settledNodes.size()-1)))
+				updateNode(e.getStartNode(),settledNodes.get(settledNodes.size()-1),e.getWeight());
+		}
+	}
+	
+	public Vector<Node> getNodes() {
+		return nodes;
+	}
+
+
+	private void updateNode(Node e, Node a, int weight)
+	{
+		if(unSettledNodes.contains(e))
+			System.out.println("nodo di partenza: "+a.getLabel()+ " nodo arrivo: "+e.getLabel()+" peso: "+weight + " distanza " + e.getDistance());
+			if(e.getDistance() > (a.getDistance() + weight)){
+				e.setDistance((a.getDistance() + weight));
+				e.setPreviousNode(a);
+				System.out.println("previous node: "+ e.getPreviousNode());
+				System.out.println("nuova distanza: "+ e.getDistance());
+				
+			}
+	}
+	private Node takeNextNode()
+	{
+		Node nextNode= null;
+		for (Node n: unSettledNodes)
+		{
+			
+			if (nextNode == null || nextNode.getDistance()>n.getDistance())
+				nextNode=n;
+		}
+		System.out.println("nodo con distanza minima: " + nextNode.getLabel());
+		return nextNode;
+	}
+	private void createPath(){
+		Node endnodo = endNodo;
+		while(!endnodo.getStart()){
+			pathD.add(endnodo.getLabel());
+			endnodo = endnodo.getPreviousNode();
+		}
+		pathD.add(endnodo.getLabel());
+	}
+	private void printPath(){
+		for(int i=(pathD.size()-1); i>=0 ; i--){
+			System.out.println(pathD.get(i));
+		}
+	}
+	public void dijkstra()
+	{
+		updateArray();
+		while (unSettledNodes.size()>0)
+		{
+			updateNearNodes();
+			settledNodes.add(takeNextNode());
+			for(Node n: settledNodes) System.out.println("SettledNodes " + n.getLabel() + "\n");
+			unSettledNodes.remove(takeNextNode());
+			for(Node n: unSettledNodes) System.out.println("unSettledNodes " + n.getLabel() + "\n");
+			
+		}
+		createPath();
+		printPath();
+		
+	}
 	
 	public String toString(){
 		return "";
